e970e2b21a3432d365ac2db47ea193792dae4318
Vara Srinivas <vara.srinivas@gmail.com>
2022-05-21 09:52:09 +0530

    additional backend changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,7 +5,7 @@ export class AppService {
   constructor(private readonly HttpService: HttpService){}
   async getData(): Promise<any> {
     let response: null;
-    response = await (await this.HttpService.get('https://dummyjson.com/products/1').toPromise()).data;
+    response = await (await this.HttpService.get('http://backend-1-service.default.svc.cluster.local').toPromise()).data;
     console.log(response);
     return response;
   }
